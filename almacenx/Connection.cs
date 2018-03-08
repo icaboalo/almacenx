04109b0c4c16864fba22927222569a06551aeaf1
IÑAKI CABO ALONSO <icaboalo@itam.mx>
2018-03-08 12:12:21 -0600

    a

---
+++ almacenx/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace almacenx
+{
+    class Connection
+    {
+
+        public static SqlConnection addConnection()
+        {
+            SqlConnection con;
+            try
+            {
+                con = new SqlConnection("");
+                con.Open();
+            } catch (Exception e)
+            {
+                con = null;
+                MessageBox.Show("No se pudo contectar " + e.Message);
+            }
+            return con;
+        }
+
+
+    }
+}
